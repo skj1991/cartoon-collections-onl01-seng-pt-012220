0a2c603fe004145ed669b1ae08b25ea2becfcdd1
Stacy Johns <stacykjohns@icloud.com>
2020-01-30 22:07:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ end
 def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.find do item
+  array.find do |item|
     cheese_types.include?(item)
   end
 end
