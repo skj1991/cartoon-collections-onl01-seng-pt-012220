8400fec7d307022150cc7e5e3caeab281d28a1e6
Stacy Johns <stacykjohns@icloud.com>
2020-01-30 22:07:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,10 @@ def long_planeteer_calls(calls)
   calls.any? { |word| word.size > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do item
+    cheese_types.include?(item)
+  end
 end
