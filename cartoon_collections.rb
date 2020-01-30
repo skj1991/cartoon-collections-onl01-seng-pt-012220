04946729a7275df3562e9fa47b4671db6947bd2d
Stacy Johns <stacykjohns@icloud.com>
2020-01-30 21:54:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,8 +4,10 @@ def roll_call_dwarves(dwarves)
   end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(calls)
+  calls.collect do |word|
+
+  end
 end
 
 def long_planeteer_calls# code an argument here
